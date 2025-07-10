05bdbbc2c3e01f762ad92d8336dd23e585590b75
jbr284 <jbrosa2009@gmail.com>
2025-07-10 00:41:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('saripan-cache').then(cache => {
-      return cache.addAll(['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png']);
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon-192x192.png',
+        'icon-512x512.png'
+      ]);
     })
   );
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
