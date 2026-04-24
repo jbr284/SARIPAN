ef649426f1e3a85fae4ad473fd9dac29891b97c1
jbr284 <jbrosa2009@gmail.com>
2026-04-23 22:41:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Aumentamos a versão para forçar a atualização do cache com as novas Abas
-const CACHE_NAME = 'gestao-unificada-v5'; 
+const CACHE_NAME = 'gestao-unificada-v6'; 
 
 const ASSETS = [
   './',
